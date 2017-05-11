3cdf2a3f797d32d8f91b0f8562e6ea0bfbcfd8d6
David <david_elvir@unitec.edu>
2017-05-11 15:15:57 -0600

    clase dinosaurio

---
+++ Dinosaurio.h
@@ -0,0 +1,16 @@
+#ifndef DINOSAURIO_H
+#define DINOSAURIO_H
+
+#include <string>
+
+class Dinosaurio{
+
+  public:
+    string nombre;
+    double altura;
+    double peso;
+    string sexo;
+    double largo;    
+};
+
+#endif 
