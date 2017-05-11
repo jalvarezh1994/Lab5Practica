74f43fe8974c1e78175d922ed0a6592d7778058f
David <david_elvir@unitec.edu>
2017-05-11 16:03:02 -0600

    din cpp

---
+++ Dinosaurio.cpp
@@ -0,0 +1,55 @@
+#include "Dinosaurio.h"
+#include <string>
+#include "Persona.h"
+using namespace std;
+
+  Dinosaurio::Dinosaurio();
+  
+  Dinosaurio::Dinosaurio(string nombre,double altura,double peso, string sexo, double largo){
+    this->nombre = nombre;
+    this->altura = altura;
+    this->peso = peso;
+    this->sexo = sexo;
+    this->largo = largo;
+  }
+
+  string Dinosaurio::getNombre(){
+    return nombre;
+  }
+
+  double Dinosaurio::getAltura(){
+    return altura;
+  }
+  double Dinosaurio::getPeso(){
+    return peso;
+  }
+  string Dinosaurio::getSexo(){
+    return sexo;
+  }
+  double Dinosaurio::getLargo(){
+    return largo;
+  }
+  Persona Dinosaurio::getCuidador(){
+    return this->cuidador;
+  }
+
+  void Dinosaurio::setNombre(string pNombre){
+    nombre = pNombre;
+  }
+  void Dinosaurio::setAltura(double pAltura){
+    altura = pAltura;
+  }
+  void Dinosaurio::setPeso(double pPeso){
+    peso = pPeso;
+  }
+  void Dinosaurio::setSexo(string pSexo){
+    sexo = pSexo;
+  }
+  void Dinosaurio::setLargo(double pLargo){
+    largo = pLargo;
+  }
+  void Dinosaurio::setCuidador(Persona pCuidador){
+    cuidador = pCuidador;
+  }
+
+
--- Dinosaurio.h
+++ Dinosaurio.h
@@ -2,6 +2,8 @@
 #define DINOSAURIO_H
 
 #include <string>
+#include "Persona.h"
+using namespace std;
 
 class Dinosaurio{
 
@@ -10,7 +12,25 @@ class Dinosaurio{
     double altura;
     double peso;
     string sexo;
-    double largo;    
+    double largo;
+    Persona cuidador;
+
+    Dinosaurio();
+    Dinosaurio(string, double,double,string,double);
+
+    string getNombre();
+    double getAltura();
+    double getPeso();
+    string getSexo();
+    double getLargo();
+    Persona getCuidador();
+
+    void setNombre(string);
+    void setAltura(double);
+    void setPeso(double);
+    void setSexo(string);
+    void setLargo(double);
+    void setCuidador(Persona);
 };
 
 #endif 
