f1a3b2ecb441be0a67a4c8a53d9213162655071c
Jorge <j.alvarezh1994@unitec.edu>
2017-05-11 09:48:50 -0600

    Avances en el main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <vector>
 #include "Dinosaurio.h"
 #include "Persona.h"
 
 using namespace std;
-
+vector<Dinosaurio> 
 
 int main(){
 	cout<<"Menú"<<endl;
@@ -14,13 +15,25 @@ int main(){
         int opc;
         switch(opc){
                 case 1:{
+			cout<<"¿Qué desea agregar?"<<endl;
+			cout<<"1. Persona"<<endl;
+			cout<<"2. Dinosaurio"<<endl;
                 }
                 case 2:{
+			cout<<"¿Qué desea listar?"<<endl;
+			cout<<"1. Personas"<<endl;
+			cout<<"2. Dinosaurio"<<endl;
                 }
                 case 3:{
+			cout<<"Modificar"<<endl;
+			cout<<"1. Persona"<<endl;
+			cout<<"2. Dinosaurio"<<endl;
                 }
                 case 4:{
+			cout<<"¿Qué desea eliminar?"<<endl;
+			cout<<"1. Persona"<<endl;
+			cout<<"2. Dinosaurio"<<endl;
                 }
         }
-	return 0;	
+	return 0;
 }
