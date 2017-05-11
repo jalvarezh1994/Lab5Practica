e3009c25a2ba9cb1ce36382d42b08893ffb1f95a
Jorge <j.alvarezh1994@unitec.edu>
2017-05-11 09:34:47 -0600

    Main creado

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "Dinosaurio.h"
+#include "Persona.h"
+
+using namespace std;
+
+
+int main(){
+	cout<<"Menú"<<endl;
+        cout<<"1. Agregar"<<endl;
+        cout<<"2. Listar"<<endl;
+        cout<<"3. Modificar"<<endl;
+        cout<<"4. Eliminar"<<endl;
+        int opc;
+        switch(opc){
+                case 1:{
+                }
+                case 2:{
+                }
+                case 3:{
+                }
+                case 4:{
+                }
+        }
+	return 0;	
+}
